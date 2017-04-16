f8bd46297db090baf820df6656014069b7aeee6a
张博群 <383088696@qq.com>
2017-04-16 15:06:53 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import App from './App.vue';
+import Element from 'element-ui';
+
+import './styles/index.css';
+
+
+
+new Vue({ // eslint-disable-line
+    el: '#app',
+    render: h => h(App)
+});
